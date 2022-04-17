f53870642673655e2ffd320f45011b8435369388
sperjar <65524316+sperjar@users.noreply.github.com>
2022-04-17 18:14:38 +0800

    feat: 基础几个功能函数实现

--- src/parser/parser.py
+++ src/parser/parser.py
@@ -1,7 +1,7 @@
 import json
-from src.web.web_rpa import web_functions
+from src.web.web_rpa import web_functions_easy as web
 
-all_functions = web_functions
+all_functions = web
 
 
 def load_json(file_path):
@@ -30,6 +30,29 @@ def run_program(program):
         func_args = step["paras"]
         if func_name in all_functions:
             all_functions[func_name](**func_args)
+
+def run_program_from_file(file_path):
+    """
+    Runs a program from a json file.
+    """
+    json_data = load_json(file_path)
+    print("[Program loaded]", json_data["name"])
+    program = load_program(json_data)
+    run_program(program)
+
+# class RpaProgram:
+#     def __init__(self):
+#         self.program = []
+#         self.name = ""
+#
+#     def load_json(self, file_path):
+#         with open(file_path, "r") as f:
+#             data = json.load(f)
+#         self.program = data["program"]
+#
+#     def run(self):
+#         for step in self.program:
+#
 
 
 if __name__ == "__main__":
