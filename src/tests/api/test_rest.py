da14459fb22ebc45303997ac86fa85d3021c49fa
sperjar <65524316+sperjar@users.noreply.github.com>
2022-07-23 20:30:39 +0800

    test(rest): add restapi test

---
+++ src/tests/api/test_rest.py
@@ -0,0 +1,26 @@
+import json
+
+from fastapi.testclient import TestClient
+
+from src.api.rest import ProgramModel
+from src.api.rest import app
+
+client = TestClient(app)
+
+
+def test_read_main():
+    response = client.get("/test")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello World"}
+
+
+def test_program_model():
+    @app.post("/test/program")
+    async def get_program_model(program: ProgramModel):
+        return program
+
+    with open("src/tests/sample.json", "r") as f:
+        data = json.load(f)
+    response = client.post("/test/program", json=data)
+    assert response.status_code == 200
+    assert response.json() == data
