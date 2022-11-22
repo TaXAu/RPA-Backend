ca602c1f713c76c9f43b8d7935e73cf83f923bf2
sperjar <65524316+sperjar@users.noreply.github.com>
2022-11-22 15:04:36 +0800

    refactor: add main entry and remove old actions

--- src/parser/parser.py
+++ src/parser/parser.py
@@ -1,21 +1,16 @@
 from typing import Optional
 from src.types import TaskModel
-from src.web.web_rpa import web_functions_easy as web
-
-all_functions = web
 
 
 class Parser(object):
-    def __init__(self, task: Optional[TaskModel]):
+    def __init__(self, task: Optional[TaskModel], toml_file: str, json_file: str):
         self.id = task.id
         self.name = task.name
         self.program = task.program
+        self.vars = {}
+
+    def step(self):
+        pass
 
     def run(self):
-        for step in self.program:
-            func_name = step.name
-            func_paras = step.paras
-            if func_name in all_functions:
-                all_functions[func_name](**func_paras)
-            else:
-                raise Exception(f"function '{func_name}' not found")
+        pass
